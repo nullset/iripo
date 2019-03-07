92324ecf578c83d91f1bfedd71b4e13f2efabf38
Nathan Wright <nathan@brandalism.com>
2019-03-07 23:18:01 +0000

    processing each existo when fn is first defined

--- jivequery.js
+++ jivequery.js
@@ -3,46 +3,52 @@ const matchMap = window.matchMap = new Map();
 const executedMap = window.executedMap = new WeakMap();
 
 function existo(selector, fn) {
-  const selectorMatch = matchMap.get(selector);
-  if (selectorMatch) {
-    selectorMatch.push(fn);
-    matchMap.set(selector, selectorMatch);
-  } else {
-    matchMap.set(selector, [fn]);
-  }
+  console.log('existo', selector)
+  const selectorActions = matchMap.get(selector) || new Set();
+  matchMap.set(selector, selectorActions.add(fn));
+  process();
+  // if (selectorMatch) {
+  //   selectorMatch.add(fn);
+  //   matchMap.set(selector, selectorMatch);
+  // } else {
+  //   matchMap.set(selector, [fn]);
+  // }
 }
 
 existo('p', (elem) => {
-  console.log('--- found', elem)
+  elem.dataset.time = Date()
 });
+
+existo.remove = (selector, fn) => {
+  // matchMap.
+}
 
 
 
 function process() {
   console.log('processing')
-  requestAnimationFrame(() => {
+  // requestAnimationFrame(() => {
     console.log('anim')
     matchMap.forEach((fns, selector) => {
       document.querySelectorAll(selector).forEach((elem) => {
         fns.forEach((fn) => {
           // fn(elem);
           const executedSet = executedMap.get(elem);
-          if (executedSet && executedSet.has(fn)) {
-            // DO NOTHING
-          } else {
+          if (!executedSet || !executedSet.has(fn)) {
             const set = executedSet || new Set();
             executedMap.set(elem, set.add(fn));
             fn(elem);
           }
         });
       });
-    });
+    // });
     // process()111`;
   });
 }
 
-const obs = new MutationObserver(() => {
+const obs = new MutationObserver((mutations, obsverver) => {
   process();
+  obsverver.takeRecords();
 });
 
 obs.observe(document.body, {

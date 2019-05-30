35655123ba5db3bcabe4dd4c24d6c848e5fc393f
Nathan Wright <nathan@brandalism.com>
2019-05-29 22:59:52 -0500

    cleanup

--- existo.js
+++ existo.js
@@ -2,18 +2,6 @@ const matchMap = window.matchMap = new Map();
 const removeMatchMap = window.removeMatchMap = new Map();
 const executedMap = window.executedMap = new WeakMap();
 
-// function existo(selector, addFn, removeFn) {
-//   const selectorActions = matchMap.get(selector) || new Set();
-//   matchMap.set(selector, selectorActions.add(addFn));
-//   processAdds();
-
-//   if (removeFn) {
-//     const removeSelectorActions = removeMatchMap.get(selector) || new Set();
-//     removeMatchMap.set(selector, removeSelectorActions.add(removeFn));
-//     // Removes are not processed on creation, as it doesn't make sense to do so.
-//   }
-// }
-
 const existo = window.existo = {
   onAdd: (selector, fn, opts = { processNow: true }) => {
     const selectorActions = matchMap.get(selector) || new Set();
@@ -41,63 +29,47 @@ const existo = window.existo = {
   }
 }
 
-
-// existo('p', (elem) => {
-//   elem.dataset.time = Date()
-// }, (elem) => {
-//   debugger;
-// });
-
-// existo('p[data-time]', (elem) => {
-//   elem.dataset.foo = 'foo'
-// })
-
-existo.onAdd('p', (elem) => {
-  elem.dataset.num2 = 'blah';
-})
-
-
 function processAdds() {
-  console.log('processing adds')
-  const allSelectors = Array.from(matchMap.keys()).join(',');
-  document.querySelectorAll(allSelectors).forEach((elem) => {
-    matchMap.forEach((fns, selector) => {
-      if (elem.matches(selector)) {
-        fns.forEach((fn) => {
-          // fn(elem);
-          const executedSet = executedMap.get(elem);
-          if (!executedSet || !executedSet.has(fn)) {
-            const set = executedSet || new Set();
-            executedMap.set(elem, set.add(fn));
-            fn(elem);
-          }
-        });
-      }
+  if (matchMap.size > 0) {
+    const allSelectors = Array.from(matchMap.keys()).join(',');
+    document.querySelectorAll(allSelectors).forEach((elem) => {
+      matchMap.forEach((fns, selector) => {
+        if (elem.matches(selector)) {
+          fns.forEach((fn) => {
+            const executedSet = executedMap.get(elem);
+            if (!executedSet || !executedSet.has(fn)) {
+              const set = executedSet || new Set();
+              executedMap.set(elem, set.add(fn));
+              fn(elem);
+            }
+          });
+        }
+      });
     });
-  });
-
+  }
 }
 
 function processRemoves(mutations) {
-  mutations.forEach((mutation) => {
-    if (mutation.removedNodes.length > 0) {
-      console.log(mutation)
-      Array.from(removeMatchMap.entries()).forEach(([selector, removeFns]) => {
-        Array.from(mutation.removedNodes).forEach((node) => {
-          if (node.nodeType === 1) {
-            const matchingNodes = new Set();
-            if (node.matches(selector)) matchingNodes.add(node);
-            Array.from(node.querySelectorAll(selector)).forEach((childNode) => {
-              matchingNodes.add(node);
-            });
-            matchingNodes.forEach((node) => {
-              removeFns.forEach((removeFn) => removeFn(node));
-            })
-          }
-        })
-      });
-    }
-  });
+  if (matchMap.size > 0) {
+    mutations.forEach((mutation) => {
+      if (mutation.removedNodes.length > 0) {
+        Array.from(removeMatchMap.entries()).forEach(([selector, removeFns]) => {
+          Array.from(mutation.removedNodes).forEach((node) => {
+            if (node.nodeType === 1) {
+              const matchingNodes = new Set();
+              if (node.matches(selector)) matchingNodes.add(node);
+              Array.from(node.querySelectorAll(selector)).forEach((childNode) => {
+                matchingNodes.add(node);
+              });
+              matchingNodes.forEach((node) => {
+                removeFns.forEach((removeFn) => removeFn(node));
+              })
+            }
+          })
+        });
+      }
+    });
+  }
 }
 
 const obs = new MutationObserver((mutations, observer) => {

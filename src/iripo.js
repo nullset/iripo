ff88537bd57bc5fc148edcbcf7545bc5a941b0a9
Nathan Wright <nathan@brandalism.com>
2019-06-02 16:38:06 -0500

    cleanup, rename methods

--- src/iripo.js
+++ src/iripo.js
@@ -1,104 +1,86 @@
-const inFns = (window.inFns = new Map());
-const outFns = (window.outFns = new Map());
-const processedInFns = (window.processedInFns = new WeakMap());
-
-const allFns = new Map();
-const pausedFns = new Map();
-
 const iripo = {
   counter: 0,
-  // inFns: new Map(),
-  // outFns: new Map(),
-  // processInFns: new WeakMap();
-  // allFns: new Map(),
+  allFns: new Map(),
+  pausedFns: new Map(),
+  inWatchers: new Map(),
+  outWatchers: new Map(),
+  processedElems: new WeakMap(),
 
-  getFnId: function getFnId(fn) {
-    const match = Array.from(allFns.entries()).find(function (entry) {
+  getSymbol: function getSymbol(fn) {
+    const match = Array.from(iripo.allFns.entries()).find(function (entry) {
       if (fn.toString() === entry[1].toString()) {
         return true;
       }
     });
     return match && match[0];
   },
-  getFn: function getFn(id) {
-    return allFns.get(id);
+  getFn: function getFn(symbol) {
+    return iripo.allFns.get(symbol);
   },
   setAction: function setAction(selector, fn, typeFns) {
     const actions = typeFns.get(selector) || new Set();
 
-    let fnId = iripo.getFnId(fn);
+    let fnId = iripo.getSymbol(fn);
 
     if (!fnId) {
       fnId = Symbol(iripo.counter);
-      allFns.set(fnId, fn);
+      iripo.allFns.set(fnId, fn);
       typeFns.set(selector, actions.add(fnId));
       iripo.counter++;
     }
     return fnId;
   },
   in: function inFn(selector, fn, opts = { processNow: true }) {
-    const id = iripo.setAction(selector, fn, inFns);
+    const id = iripo.setAction(selector, fn, iripo.inWatchers);
     if (opts.processNow) iripo.processInFns();
     return id;
   },
   out: function outFn(selector, fn) {
-    return iripo.setAction(selector, fn, outFns);
+    return iripo.setAction(selector, fn, iripo.outWatchers);
   },
-  // clearFn: function clearFn(typeFns, selector, fn) {
-  //   const fns = typeFns.get(selector);
-  //   if (typeof fn === "function") {
-  //     fns.delete(fn);
-  //   } else {
-  //     elemMap.set(
-  //       selector,
-  //       Array.from(fns).filter(matchFn => matchFn.toString() !== fn.toString())
-  //     );
-  //   }
-  // },
   clear: function removeInFn(symbol) {
-    // iripo.clearFn(symbol);
-    allFns.delete(symbol);
-    pausedFns.delete(symbol);
-    [inFns, outFns].forEach(function (typeFns) {
+    iripo.allFns.delete(symbol);
+    iripo.pausedFns.delete(symbol);
+    [iripo.inWatchers, iripo.outWatchers].forEach(function (typeFns) {
       typeFns.forEach(function (actions) {
         actions.delete(symbol);
       });
     });
   },
   pause: function pause(symbol) {
-    pausedFns.set(symbol, true);
+    iripo.pausedFns.set(symbol, true);
     return symbol;
   },
   pauseAll: function pauseAll() {
-    allFns.forEach(function (fn, symbol) {
+    iripo.allFns.forEach(function (fn, symbol) {
       iripo.pause(symbol);
     });
   },
   resume: function resume(symbol, processNow = true) {
-    pausedFns.delete(symbol);
+    iripo.pausedFns.delete(symbol);
     if (processNow) iripo.processInFns();
     return symbol;
   },
   resumeAll: function resumeAll() {
-    allFns.forEach(function (fn, symbol) {
+    iripo.allFns.forEach(function (fn, symbol) {
       iripo.resume(symbol, false);
     });
     iripo.processInFns();
   },
   processInFns: function processInFns() {
     console.log("process");
-    if (inFns.size > 0) {
-      const allSelectors = Array.from(inFns.keys()).join(",");
+    if (iripo.inWatchers.size > 0) {
+      const allSelectors = Array.from(iripo.inWatchers.keys()).join(",");
       document.querySelectorAll(allSelectors).forEach(function (elem) {
-        inFns.forEach(function (fnIds, selector) {
+        iripo.inWatchers.forEach(function (fnIds, selector) {
           if (elem.matches(selector)) {
-            fnIds.forEach(function (id) {
-              if (!pausedFns.get(id)) {
-                const processedInActions = processedInFns.get(elem);
-                if (!processedInActions || !processedInActions.has(id)) {
-                  const fn = iripo.getFn(id);
+            fnIds.forEach(function (symbol) {
+              if (!iripo.pausedFns.get(symbol)) {
+                const processedInActions = iripo.processedElems.get(elem);
+                if (!processedInActions || !processedInActions.has(symbol)) {
+                  const fn = iripo.getFn(symbol);
                   const set = processedInActions || new Set();
-                  processedInFns.set(elem, set.add(id));
+                  iripo.processedElems.set(elem, set.add(symbol));
                   fn(elem);
                 }
               }
@@ -109,10 +91,10 @@ const iripo = {
     }
   },
   processOutFns: function processOutFns(mutations) {
-    if (outFns.size > 0) {
+    if (iripo.outWatchers.size > 0) {
       mutations.forEach(function (mutation) {
         if (mutation.removedNodes.length > 0) {
-          outFns.forEach(function (fnIds, selector) {
+          iripo.outWatchers.forEach(function (fnIds, selector) {
             Array.from(mutation.removedNodes).forEach(function (node) {
               if (node.nodeType === 1) {
                 const matchingNodes = new Set();
@@ -123,9 +105,9 @@ const iripo = {
                   matchingNodes.add(childNode);
                 });
                 matchingNodes.forEach(function (node) {
-                  fnIds.forEach(function (id) {
-                    if (!pausedFns.get(id)) {
-                      const fn = iripo.getFn(id);
+                  fnIds.forEach(function (symbol) {
+                    if (!iripo.pausedFns.get(symbol)) {
+                      const fn = iripo.getFn(symbol);
                       fn(node);
                     }
                   });

0458ded59e82fce90ba19276e271f901f924618f
Nathan Wright <nathan@brandalism.com>
2019-06-02 13:01:10 -0500

    wip - store all fns in a common Map, reference them by id
    This should drastically reduce the amount of data stored in iripo

--- src/iripo.js
+++ src/iripo.js
@@ -2,15 +2,41 @@ const inFns = (window.inFns = new Map());
 const outFns = (window.outFns = new Map());
 const processedInFns = (window.processedInFns = new WeakMap());
 
+let fnIdCounter = 0;
+const allFns = new Map();
+
 const iripo = {
+  counter: 0,
+  getFnId: function getFnId(fn) {
+    const match = Array.from(allFns.entries()).find(function (entry) {
+      if (fn.toString() === entry[1].toString()) {
+        return true;
+      }
+    });
+    return match && match[0];
+  },
+  getFn: function getFn(id) {
+    return allFns.get(id);
+  },
+  setAction: function getOrCreateFn(selector, fn, typeFns) {
+    const actions = typeFns.get(selector) || new Set();
+
+    let fnId = iripo.getFnId(fn);
+
+    if (!fnId) {
+      iripo.counter++;
+      fnId = iripo.counter;
+      allFns.set(fnId, fn);
+    }
+
+    typeFns.set(selector, actions.add(fnId));
+  },
   in: function inFn(selector, fn, opts = { processNow: true }) {
-    const inActions = inFns.get(selector) || new Set();
-    inFns.set(selector, inActions.add(fn));
+    iripo.setAction(selector, fn, inFns);
     if (opts.processNow) iripo.processInFns();
   },
   out: function outFn(selector, fn) {
-    const outActions = outFns.get(selector) || new Set();
-    outFns.set(selector, outActions.add(fn));
+    iripo.setAction(selector, fn, outFns);
   },
   clearFn: function clearFn(elemMap, selector, fn) {
     const fns = elemMap.get(selector);
@@ -33,14 +59,15 @@ const iripo = {
     console.log("process");
     if (inFns.size > 0) {
       const allSelectors = Array.from(inFns.keys()).join(",");
-      document.querySelectorAll(allSelectors).forEach(function(elem) {
-        inFns.forEach(function(fns, selector) {
+      document.querySelectorAll(allSelectors).forEach(function (elem) {
+        inFns.forEach(function (fnIds, selector) {
           if (elem.matches(selector)) {
-            fns.forEach(function(fn) {
+            fnIds.forEach(function (id) {
               const processedInActions = processedInFns.get(elem);
-              if (!processedInActions || !processedInActions.has(fn)) {
+              if (!processedInActions || !processedInActions.has(id)) {
+                const fn = iripo.getFn(id);
                 const set = processedInActions || new Set();
-                processedInFns.set(elem, set.add(fn));
+                processedInFns.set(elem, set.add(id));
                 fn(elem);
               }
             });
@@ -51,20 +78,21 @@ const iripo = {
   },
   processOutFns: function processOutFns(mutations) {
     if (outFns.size > 0) {
-      mutations.forEach(function(mutation) {
+      mutations.forEach(function (mutation) {
         if (mutation.removedNodes.length > 0) {
-          outFns.forEach(function(fns, selector) {
-            Array.from(mutation.removedNodes).forEach(function(node) {
+          outFns.forEach(function (fnIds, selector) {
+            Array.from(mutation.removedNodes).forEach(function (node) {
               if (node.nodeType === 1) {
                 const matchingNodes = new Set();
                 if (node.matches(selector)) matchingNodes.add(node);
-                Array.from(node.querySelectorAll(selector)).forEach(function(
+                Array.from(node.querySelectorAll(selector)).forEach(function (
                   childNode
                 ) {
                   matchingNodes.add(childNode);
                 });
-                matchingNodes.forEach(function(node) {
-                  fns.forEach(function(fn) {
+                matchingNodes.forEach(function (node) {
+                  fnIds.forEach(function (id) {
+                    const fn = iripo.getFn(id);
                     fn(node);
                   });
                 });
